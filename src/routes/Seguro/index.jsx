137d43e6e30cb7766b943724a06a2a3d4a91e6ce
Laura Trujillo Rosal <lauratrugillo@gmail.com>
2023-09-09 01:09:13 -0300

    Seguro 2

--- src/routes/Seguro/index.jsx
+++ src/routes/Seguro/index.jsx
@@ -1,5 +1,9 @@
 import fundo from '../../img/plano_fundo_2.png'
 import check from '../../img/check.png'
+import exclamacao_icon from '../../img/exclamacao_icon.png'
+import acidente_icon from '../../img/acidente_icon.png'
+import bicicleta_icon from '../../img/bicicleta_icon.png'
+import grupo_icon from '../../img/grupo_icon.png'
 
 export default function Seguro(){
 
@@ -20,12 +24,31 @@ export default function Seguro(){
             </div>
         </div>
         <br />
-        <div class='container3'>
-        <h2>Com o Porto Seguro Bike, você escolhe as <br />coberturas de acordo com as suas <br />necessidades.</h2>
-        <br />
-        <p>São diversas coberturas para você <br />
-        só se preocupar em pedalar:</p>
 
+        <div class='container3'>
+            <h2>Com o Porto Seguro Bike, você escolhe as <br />coberturas de acordo com as suas <br />necessidades.</h2>
+            <br />
+            <p>São diversas coberturas para você <br />
+            só se preocupar em pedalar:</p>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <img src={exclamacao_icon} alt="Card 1" />
+                <h2>Danos a bike</h2>
+            </div>
+            <div class="card">
+                <img src={bicicleta_icon} alt="Card 2"/>
+                <h2>Roubo</h2>
+            </div>
+            <div class="card">
+                <img src={acidente_icon} alt="Card 3"/>
+                <h2>Acidentes pessoais</h2>
+            </div>
+            <div class="card">
+                <img src={grupo_icon} alt="Card 4"/>
+                <h2>Danos a terceiros</h2>
+            </div>
         </div>
         </main>
     )
