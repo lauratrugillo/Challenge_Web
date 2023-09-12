edd670c5fd0b098442492737c6295e46361769dc
Nicole Ribeiro Tuccio <98128263+NicoleTuccio@users.noreply.github.com>
2023-09-12 15:02:05 -0300

    Update index.jsx

--- src/routes/Cadastro/index.jsx
+++ src/routes/Cadastro/index.jsx
@@ -3,6 +3,27 @@ export default function Cadastro(){
     return(
         <main>
             <h1>Cadastro</h1>
+
+            <h3>Ligamos para você</h3>
+            <h4>Sua Bicicleta</h4>
+
+            <form action="#" method="POST">
+            
+                <input type="text" id="valorBicicleta" name="valorBicicleta" placeholder="Qual o valor da sua bicicleta?" required ><br><br>
+            
+                <input type="text" id="nome" name="nome" placeholder="Nome" required><br><br>
+
+                <input type="text" id="email" name="email" placeholder="E-mail" required><br><br>
+
+                <input type="text" id="telefone" name="telefone" placeholder="Telefone para contato" required><br><br>
+
+                <input type="text" id="cpf" name="cpf" placeholder="CPF" required><br><br>
+
+                <input type="text" id="cep" name="cep" placeholder="CEP" required><br><br>
+
+                <button type="submit">Continuar</button>
+
+            </form>
         </main>
     )
 }
