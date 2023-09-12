b1f6454e6c590a77df86ee235ccbdfbf40b0a517
ThiagoTodeschini <126583151+ThiagoTodeschini@users.noreply.github.com>
2023-09-12 17:09:10 -0300

    Update index.jsx

--- src/routes/Vistoria/index.jsx
+++ src/routes/Vistoria/index.jsx
@@ -1,8 +1,18 @@
+import camera from '../../img/camera.png'
+
 export default function Vistoria(){
 
     return(
         <main>
-            <h1>Vistoria</h1>
-        </main>
-    )
+            <div class='container4'>
+                <center>
+                    <h1 id='vistoria_computador'>Por favor, realize esta etapa utilizando um aparelho móvel.</h1>
+                    <h1 id='vistoria_movel'>Siga as intruções exibidas na tela</h1>
+                    <div class='container5'>
+                        <img src={camera} alt="" />
+                    </div>
+                </center>
+            </div>
+        </main>
+    )
 }
